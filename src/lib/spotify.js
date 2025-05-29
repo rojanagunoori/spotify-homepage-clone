bcef785f36e6ccb2a68b11bb9584b57c33eaf897
rojanagunoori <nagunooriroja@gmail.com>
2025-05-29 11:03:59 +0530

    track

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -13,22 +13,16 @@ export const getUserPlaylists = async (accessToken) => {
   };
 
 
-
-  export async function getUserSavedTracks(accessToken) {
-    console.log(accessToken)
-    const response = await fetch("https://api.spotify.com/v1/me/tracks", {
+  export const  getUserSavedTracks = async (accessToken) => {
+    const res = await fetch("https://api.spotify.com/v1/me/tracks", {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    console.log(response)
-    if (!response.ok) {
-     
-      throw new Error("Failed to fetch saved tracks",response);
-    }
-    const data = await response.json();
-    console.log("Tracks: ",Tracks)
-    return data.items; // Each item contains { track: {...} }
-  }
   
+    if (!res.ok) throw new Error("Failed to fetch playlists");
   
+    const data = await res.json();
+    console.log("Tracks: ", data);
+    return data.items;
+  };
